fee312f39aff8f113aa806b94c5a16648c9ede47
Rina <37088203+MrsRina@users.noreply.github.com>
2023-08-16 16:16:52 -0300

    Update compilar-win32.bat

--- demo/compilar-win32.bat
+++ demo/compilar-win32.bat
@@ -3,11 +3,11 @@
 
 :: S é de SOURCE, o . atual diz a pasta que o CMakeLists.txt está.
 :: B é de BUILD, e diz aonde queremos que o CMake gere arquivos makefile.
-:: G significa qual tipo de makefile para o toolchain (compilador),
-:: nesse caso é o ninja, mas poderiamos gerar makefiles com "MinGW Makefiles",
-:: ou "Unix Makefiles" para sistema operacionais Linux.
+:: G significa qual tipo de makefile para o toolchain (compilador) gerar,
+:: poderiamos gerar makefiles com "MinGW Makefiles", ou
+:: "Unix Makefiles" para sistema operacionais Linux.
 
-cmake -S . -B ./cmake-build-debug -G "Ninja"
+cmake -S . -B ./cmake-build-debug -G "MinGW Makefiles"
 
 :: --build é um comando para pegar tudo que foi construido pelo comando acima,
 :: e compilar o programa.
